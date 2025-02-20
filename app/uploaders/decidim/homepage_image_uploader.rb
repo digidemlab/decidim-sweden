5c1d4d21896fe55ec5de42c25c44c68e13eba973
Petter <petter@digidemlab.org>
2025-02-20 09:29:48 +0100

    Startpage image size

--- app/uploaders/decidim/homepage_image_uploader.rb
+++ app/uploaders/decidim/homepage_image_uploader.rb
@@ -4,7 +4,7 @@ module Decidim
   # This class deals with uploading hero images to organizations.
   class HomepageImageUploader < RecordImageUploader
     set_variants do
-      { big: { resize_to_fill: [728, 448] } }
+      { big: { resize_to_fill: [1920, 666] } }
     end
 
     def max_image_height_or_width
