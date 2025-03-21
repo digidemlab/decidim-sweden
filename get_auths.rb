a76d007cbdc37001634c79a201921987845ac1d8
Decidim Göteborg <petter@digidemlab.org>
2025-03-21 14:59:54 +0100

    Update contact export script

--- get_auths.rb
+++ get_auths.rb
@@ -1,8 +1,12 @@
+#
+# Export contact info 
+# RAILS_ENV=production ../.rbenv/shims/rails runner get_auths.rb > ../kontaktuppgifter.csv
+#
 Decidim::Authorization.all.each do |auth|
   user = Decidim::User.find_by(id: auth.decidim_user_id)
   space = auth.metadata["participatory_space"]
   next unless space
   process_id = space.split("/")[-1]
   process = Decidim::ParticipatoryProcess.find_by(id: process_id)
-  puts "#{process.slug},#{auth.decidim_user_id},#{user.nickname},#{user.email},#{auth.metadata["phone_number"]}"
+  puts "#{process.slug};#{auth.decidim_user_id};#{user.nickname};#{user.email};#{auth.metadata["phone_number"]}"
 end
