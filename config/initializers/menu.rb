29a0852096591f9f600fd969e1d9fd14364e6278
Rupus Reinefjord <rupus@digidemlab.org>
2025-04-24 11:38:44 +0200

    display processes in mobile nav menu

---
+++ config/initializers/menu.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Decidim.menu :mobile_menu do |menu|
+  Decidim::ParticipatoryProcess.active.each do |process|
+    menu_id = "process_#{process.slug}"
+    menu.add_item menu_id,
+                  process.title[I18n.locale.to_s],
+                  "/processes/#{process.slug}"
+    menu.move menu_id, after: :participatory_processes
+  end
+end
