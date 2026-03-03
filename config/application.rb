2eb6e163ef1b14637763c6be4f3a2f1e5ae38925
Rupus Reinefjord <rupus@digidemlab.org>
2026-03-03 15:47:59 +0100

    include phone number in proposal export

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module DecidimSweden
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.to_prepare do
+      Decidim::Proposals::ProposalSerializer.include(Decidim::ProposalSerializerExtensions)
+    end
   end
 end
